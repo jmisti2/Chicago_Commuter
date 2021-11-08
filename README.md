c2acb58a2ff92ae2e03b648453c1695f9c77c135
johnmistica0 <79333427+johnmistica0@users.noreply.github.com>
2021-11-08 14:10:32 -0600

    pages/button functionality
    added pages and button functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chicago_Commuter
+
+CS 422 Chicago Commuter Web App
+
+Authors: hmenni2, imacia3, tjang7, jmisti2, jdoher8
---
+++ js/index-functions.js
@@ -0,0 +1,9 @@
+let emergBtn = document.getElementById('emergency-btn');
+emergBtn.addEventListener('click', function dothis(){
+    window.location.href = 'https://isp.illinois.gov/';
+});
+
+let settingsBtn = document.getElementById('settings-btn');
+settingsBtn.addEventListener('click', function dothis(){
+    window.location.href = '/settings.html';
+});
