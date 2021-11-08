a102decfabb80e5dd1721a54bc30f527520f9f18
johnmistica0 <79333427+johnmistica0@users.noreply.github.com>
2021-11-08 17:16:26 -0600

    report incident pop-up
    created report form

--- js/index-functions.js
+++ js/index-functions.js
@@ -1,9 +1,58 @@
+//Styling for index.html
+
+
+let modal = document.getElementById("myModal");
+
 let emergBtn = document.getElementById('emergency-btn');
 emergBtn.addEventListener('click', function dothis(){
-    window.location.href = 'https://isp.illinois.gov/';
+    modal.style.display = "block";
 });
 
 let settingsBtn = document.getElementById('settings-btn');
 settingsBtn.addEventListener('click', function dothis(){
     window.location.href = '/settings.html';
+});
+
+let submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', function dothis(){
+    modal.style.display = "none";
+});
+
+let cancelBtn = document.getElementById('cancel-btn');
+cancelBtn.addEventListener('click', function dothis(){
+    modal.style.display = "none";
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+let dropBtn = document.getElementById("down-icon");
+dropBtn.addEventListener('click', myFunction());
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+let check1btn = document.getElementById('check1');
+let check2btn = document.getElementById('check2');
+
+check1btn.addEventListener('click', function dothis(){
+    check2btn.checked = false;
+});
+
+check2btn.addEventListener('click', function dothis(){
+    check1btn.checked = false;
 });
