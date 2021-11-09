24e9ed90d75fd901f7bc7054015aca01202e3840
johnmistica0 <79333427+johnmistica0@users.noreply.github.com>
2021-11-08 23:38:39 -0600

    Update index-functions.js

--- js/index-functions.js
+++ js/index-functions.js
@@ -36,7 +36,7 @@ nobtn.addEventListener('click', function dothis(){
 
 let settingsBtn = document.getElementById('settings-btn');
 settingsBtn.addEventListener('click', function dothis(){
-    window.location.href = '/Chicago_Commuter/settings.html';
+    location.href = '/settings.html';
 });
 
 /* When the user clicks on the button,
