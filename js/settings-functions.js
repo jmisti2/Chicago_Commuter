e88b4aa71808cd7071ea9808a5034e2558b2c378
johnmistica0 <79333427+johnmistica0@users.noreply.github.com>
2021-11-12 14:57:23 -0600

    final styling
    styling for settings and index page

--- js/settings-functions.js
+++ js/settings-functions.js
@@ -1,6 +1,39 @@
+
+
+let policebtn = document.getElementById("notifyPolice");
+let contactbtn = document.getElementById("notifyContacts");
+let bothbtn = document.getElementById("notifyBoth");
+
+let notifyArr = [policebtn, contactbtn, bothbtn];
+
+// for(var i = 0; i < notifyArr.length; i++){
+//     notifyArr[i].addEventListener('click', function(){
+//         notifyArr[i].style.border="3px solid black"
+//     });
+// }
+
+policebtn.addEventListener('click', function(){
+    policebtn.style.borderWidth="3px"
+    contactbtn.style.borderWidth="1px"
+    bothbtn.style.borderWidth="1px"
+});
+
+contactbtn.addEventListener('click', function(){
+    policebtn.style.borderWidth="1px"
+    contactbtn.style.borderWidth="3px"
+    bothbtn.style.borderWidth="1px"
+});
+
+bothbtn.addEventListener('click', function(){
+
+    policebtn.style.borderWidth="1px"
+    contactbtn.style.borderWidth="1px"
+    bothbtn.style.borderWidth="3px"
+});
+
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
