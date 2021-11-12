8ee4eb4d22a867b5e53b4897ebe2c0316f4d2fd1
hmenni2 <hmenni2@uic.edu>
2021-11-12 09:25:58 -0600

    Incidents GR4 Finalized

--- js/incidents-functions.js
+++ js/incidents-functions.js
@@ -6,7 +6,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
+        var content = this.querySelector('.content');
         if (content.style.display === "block") {
             content.style.display = "none";
         } else {
